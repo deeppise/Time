e63e096c3766157282a10855df9ea30c41de06e1
deeppise <125956137+deeppise@users.noreply.github.com>
2023-02-27 10:07:38 +0530

    Add files via upload

---
+++ Action2.java
@@ -0,0 +1,25 @@
+package com.Practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Action2 {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.actitime.com/");
+		WebElement Price = driver.findElement(By.linkText("Pricing"));
+		Actions action=new Actions(driver);
+		action.moveToElement(Price, -100, 0).perform();
+	}
+
+}
